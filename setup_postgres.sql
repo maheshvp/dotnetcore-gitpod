79759de942ec57be9f60c75e98ab7037c8e17b91
Mahesh Panchanathan <maheshvp.cs@gmail.com>
2022-01-19 19:51:05 +0530

    Update setup_postgres.sql

--- setup_postgres.sql
+++ setup_postgres.sql
@@ -3,7 +3,6 @@ create user postgres password 'Portal123!';
 grant all privileges on database dotnet_dev to postgres;
 
 create database hangfire_dev;
-create user postgres password 'Portal123!';
 grant all privileges on database hangfire_dev to postgres;
 
 \c start_dev
