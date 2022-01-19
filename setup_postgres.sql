92e182f927997d0452e7c3473b10de0d6b9a4007
maheshvp <maheshvp.cs@gmail.com>
2022-01-19 14:00:05 +0000

    Update docker file to setup postgres

---
+++ setup_postgres.sql
@@ -0,0 +1,9 @@
+create database dotnet_dev;
+create user postgres password 'Portal123!';
+grant all privileges on database dotnet_dev to postgres;
+
+create database hangfire_dev;
+create user postgres password 'Portal123!';
+grant all privileges on database hangfire_dev to postgres;
+
+\c start_dev
