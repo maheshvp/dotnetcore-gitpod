0de098a59909833e90f0a35ac0aebd60845b1d04
Mahesh Panchanathan <maheshvp.cs@gmail.com>
2022-02-11 14:44:00 +0530

    Update setup_postgres.sql

--- setup_postgres.sql
+++ setup_postgres.sql
@@ -1,8 +1,8 @@
-SELECT 'CREATE DATABASE start_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'start_dev')\gexec;
+SELECT 'CREATE DATABASE start_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'start_dev')\gexec
 create user postgres password 'Portal123!';
 grant all privileges on database start_dev to postgres;
 
-SELECT 'CREATE DATABASE hangfire_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hangfire_dev')\gexec;
+SELECT 'CREATE DATABASE hangfire_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hangfire_dev')\gexec
 grant all privileges on database hangfire_dev to postgres;
 
 \c start_dev
