88cb856b131eca84ba9a39607bec59eec7ea41d8
Mahesh Panchanathan <maheshvp.cs@gmail.com>
2022-02-11 15:06:52 +0530

    Update setup_postgres.sql

--- setup_postgres.sql
+++ setup_postgres.sql
@@ -1,7 +1,7 @@
-SELECT 'CREATE DATABASE start_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'start_dev')\gexec;
-grant all privileges on database start_dev to gitpod;
+SELECT 'CREATE DATABASE start_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'start_dev')\gexec
+grant all privileges on database start_dev to gitpod
 
-SELECT 'CREATE DATABASE hangfire_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hangfire_dev')\gexec;
-grant all privileges on database hangfire_dev to gitpod;
+SELECT 'CREATE DATABASE hangfire_dev' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'hangfire_dev')\gexec
+grant all privileges on database hangfire_dev to gitpod
 
 \c start_dev
